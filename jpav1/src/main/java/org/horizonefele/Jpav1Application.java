dc614a1007419bbc0bf29abf2ea6093492836387
ananomrii@gmail <ananomrii@gmail.com>
2024-12-31 09:47:03 +0100

    finallll

---
+++ jpav1/src/main/java/org/horizonefele/Jpav1Application.java
@@ -0,0 +1,13 @@
+package org.horizonefele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpav1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpav1Application.class, args);
+	}
+
+}
